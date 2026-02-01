4da4e2174432f843e92d7f6599f8166ad6e82e53
Alejandro Fernández <alejandrofn2204@gmail.com>
2026-02-01 10:33:57 -0600

    fix: keymaps
    - Removed old/unsed keymaps.
    - Added keymap to use LSP rename.
    - Added keymap to toggle line wrap.

--- dot_config/nvim/lua/config/remap.lua
+++ dot_config/nvim/lua/config/remap.lua
@@ -10,10 +10,6 @@ keymap("n", "x", '"_x')
 keymap("n", "<leader>+", "<C-a>", { desc = "Increment number" })
 keymap("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
 
--- Scroll navigation
--- keymap("n", "<C-u>", "<C-u>zz", { desc = "Scroll up" })
--- keymap("n", "<C-d>", "<C-d>zz", { desc = "Scroll down" })
-
 -- Window split
 keymap("n", "<leader>wv", "<C-w>v", { desc = "Split window vertically" })
 keymap("n", "<leader>wh", "<C-w>s", { desc = "Split window horizontally" })
@@ -51,8 +47,8 @@ keymap("v", "<A-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv"
 keymap("n", "<leader>fn", "<cmd>enew<CR>", { desc = "New File" })
 keymap("n", "<leader>fs", "<cmd>write<CR>", { desc = "Save File" })
 keymap({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
-keymap("n", "<leader>fS", "<cmd>write<CR><cmd>quit<CR>", { desc = "Save File and Quit" })
 keymap("n", "<leader>fu", "<cmd>edit!<CR>", { desc = "Undo All Changes in File" })
+keymap("n", "<F2>", vim.lsp.buf.rename, { desc = "Power Rename"})
 
 -- Clear search and stop snippet on escape
 keymap({ "i", "n", "s" }, "<esc>", function()
@@ -69,14 +65,13 @@ vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next Diagnostic" }
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Prev Diagnostic" })
 -- stylua: ignore end
 
+-- Others
+
 -- Lazygit
 -- stylua: ignore
 keymap("n", "<leader>gg", function() Snacks.lazygit() end, { desc = "Lazygit" })
 
--- Zen mode
-keymap("n", "<leader>z", "<cmd>ZenMode<CR>", { desc = "Zen Mode" })
-
--- Others
+-- Toggle line wrap
 keymap({ "n", "v", "x" }, "<A-z>", function()
   if vim.opt.wrap then
     vim.opt.wrap = false
