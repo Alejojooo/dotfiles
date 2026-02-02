69a1133a8b02d83dc96389a66ebe3548f2e9f70f
Alejandro Fernández <alejandrofn2204@gmail.com>
2026-02-02 09:47:04 -0600

    fix: identation on wezterm.lua

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -3,23 +3,23 @@ local config = wezterm.config_builder()
 
 -- Detect system theme
 function scheme_for_appearance(appearance)
-	if appearance:find("Dark") then
-		return "Kanagawa (Gogh)" -- Dark theme
-	else
-		return "Kanagawa Lotus (Gogh)" -- Light theme
-	end
+  if appearance:find("Dark") then
+    return "Kanagawa (Gogh)" -- Dark theme
+  else
+    return "Kanagawa Lotus (Gogh)" -- Light theme
+  end
 end
 
 -- Suscribe to event that notifies system changes
 wezterm.on("window-config-reloaded", function(window, pane)
-	local overrides = window:get_config_overrides() or {}
-	local appearance = window:get_appearance()
-	local scheme = scheme_for_appearance(appearance)
+  local overrides = window:get_config_overrides() or {}
+  local appearance = window:get_appearance()
+  local scheme = scheme_for_appearance(appearance)
 
-	if overrides.color_scheme ~= scheme then
-		overrides.color_scheme = scheme
-		window:set_config_overrides(overrides)
-	end
+  if overrides.color_scheme ~= scheme then
+    overrides.color_scheme = scheme
+    window:set_config_overrides(overrides)
+  end
 end)
 
 -- Initial configuration
